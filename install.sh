5d7be2cd651fb9618f58eecc9b9c729651d7170f
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-02 18:33:19 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,21 +17,56 @@ pip install Flask Flask-SQLAlchemy
 
 # Create the main application file
 cat > app.py <<EOL
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crm.db'
 db = SQLAlchemy(app)
 
-class Customer(db.Model):
+class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    contacts = db.relationship('Contact', backref='account', lazy=True)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
+    phone = db.Column(db.String(15), nullable=True)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def dashboard():
+    total_accounts = Account.query.count()
+    total_contacts = Contact.query.count()
+    return render_template('dashboard.html', total_accounts=total_accounts, total_contacts=total_contacts)
+
+@app.route('/accounts', methods=['GET', 'POST'])
+def accounts():
+    if request.method == 'POST':
+        account_name = request.form['name']
+        new_account = Account(name=account_name)
+        db.session.add(new_account)
+        db.session.commit()
+        return redirect(url_for('accounts'))
+    accounts = Account.query.all()
+    return render_template('accounts.html', accounts=accounts)
+
+@app.route('/contacts', methods=['GET', 'POST'])
+def contacts():
+    if request.method == 'POST':
+        contact_name = request.form['name']
+        contact_email = request.form['email']
+        contact_phone = request.form['phone']
+        account_id = request.form['account']
+        new_contact = Contact(name=contact_name, email=contact_email, phone=contact_phone, account_id=account_id)
+        db.session.add(new_contact)
+        db.session.commit()
+        return redirect(url_for('contacts'))
+    contacts = Contact.query.all()
+    accounts = Account.query.all()
+    return render_template('contacts.html', contacts=contacts, accounts=accounts)
 
 if __name__ == '__main__':
     with app.app_context():
