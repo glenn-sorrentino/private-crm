0603dcb2a31e86a3d51ded188eebc9635531b717
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-02 18:52:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ source venv/bin/activate
 pip install Flask Flask-SQLAlchemy
 
 # Create the main application file
+# Create the main application file
 cat > app.py <<EOL
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -23,11 +24,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crm.db'
 db = SQLAlchemy(app)
-
-class Account(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    contacts = db.relationship('Contact', backref='account', lazy=True)
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,16 +42,29 @@ def dashboard():
     total_contacts = Contact.query.count()
     return render_template('dashboard.html', total_accounts=total_accounts, total_contacts=total_contacts)
 
+# ...
 @app.route('/accounts', methods=['GET', 'POST'])
 def accounts():
     if request.method == 'POST':
         account_name = request.form['name']
-        new_account = Account(name=account_name)
+        account_industry = request.form['industry']
+        account_location = request.form['location']
+        new_account = Account(name=account_name, industry=account_industry, location=account_location)
         db.session.add(new_account)
         db.session.commit()
         return redirect(url_for('accounts'))
     accounts = Account.query.all()
     return render_template('accounts.html', accounts=accounts)
+
+# ...
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    industry = db.Column(db.String(100), nullable=False)  # Add the industry field
+    location = db.Column(db.String(100), nullable=False)  # Add the location field
+    contacts = db.relationship('Contact', backref='account', lazy=True)
+
+# ...
 
 @app.route('/contacts', methods=['GET', 'POST'])
 def contacts():
@@ -91,6 +100,7 @@ cat > templates/dashboard.html <<EOL
     <p>Total Accounts: {{ total_accounts }}</p>
     <p>Total Contacts: {{ total_contacts }}</p>
     <nav>
+        <a href="{{ url_for('dashboard') }}">Dashboard</a>
         <a href="{{ url_for('accounts') }}">Accounts</a>
         <a href="{{ url_for('contacts') }}">Contacts</a>
     </nav>
@@ -119,6 +129,7 @@ cat > templates/accounts.html <<EOL
     </ul>
     <nav>
         <a href="{{ url_for('dashboard') }}">Dashboard</a>
+        <a href="{{ url_for('accounts') }}">Accounts</a>
         <a href="{{ url_for('contacts') }}">Contacts</a>
     </nav>
 </body>
@@ -129,37 +140,98 @@ cat > templates/contacts.html <<EOL
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Contacts</title>
+    <title>Accounts</title>
+    <style>
+        /* Add some basic CSS for the modal */
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0,0,0,0.4);
+        }
+
+        .modal-content {
+            background-color: #fefefe;
+            margin: 15% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+        }
+    </style>
 </head>
 <body>
-    <h1>Contacts</h1>
-    <form method="post" action="{{ url_for('contacts') }}">
-        <label for="name">Contact Name:</label>
-        <input type="text" id="name" name="name" required>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        <label for="phone">Phone:</label>
-        <input type="tel" id="phone" name="phone">
-        <label for="account">Account:</label>
-        <select id="account" name="account">
-            {% for account in accounts %}
-            <option value="{{ account.id }}">{{ account.name }}</option>
-            {% endfor %}
-        </select>
-        <input type="submit" value="Add Contact">
-    </form>
-    <h2>All Contacts</h2>
+    <h1>Accounts</h1>
+    <button id="new-account-btn">New Account</button>
+    <h2>All Accounts</h2>
     <ul>
-        {% for contact in contacts %}
-        <li>{{ contact.name }} - {{ contact.email }} - {{ contact.phone }} - {{ contact.account.name }}</li>
+        {% for account in accounts %}
+        <li>{{ account.name }} - {{ account.industry }} - {{ account.location }}</li>
         {% endfor %}
     </ul>
     <nav>
         <a href="{{ url_for('dashboard') }}">Dashboard</a>
-        <a href="{{ url_for('accounts') }}">Accounts</a>
+        <a href="{{ url_for('contacts') }}">Contacts</a>
     </nav>
+
+    <!-- Add the modal -->
+    <div id="new-account-modal" class="modal">
+        <div class="modal-content">
+            <h2>Create New Account</h2>
+            <form id="new-account-form">
+                <label for="name">Account Name:</label>
+                <input type="text" id="name" name="name" required>
+                <label for="industry">Industry:</label>
+                <input type="text" id="industry" name="industry" required>
+                <label for="location">Location:</label>
+                <input type="text" id="location" name="location" required>
+                <input type="submit" value="Save">
+                <button type="button" id="cancel-btn">Cancel</button>
+            </form>
+        </div>
+    </div>
+
+    <script>
+        // Get the modal, form, and buttons
+        var modal = document.getElementById('new-account-modal');
+        var btn = document.getElementById('new-account-btn');
+        var cancelBtn = document.getElementById('cancel-btn');
+        var form = document.getElementById('new-account-form');
+
+        // Open the modal when the user clicks the "New Account" button
+        btn.onclick = function() {
+            modal.style.display = 'block';
+        }
+
+        // Close the modal when the user clicks the "Cancel" button
+        cancelBtn.onclick = function() {
+            modal.style.display = 'none';
+        }
+
+        // Send a POST request when the user submits the form
+        form.onsubmit = function(e) {
+            e.preventDefault();
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '{{ url_for("accounts") }}', true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    // Refresh the page to display the new account
+                    location.reload();
+                }
+            };
+            var formData = new FormData(form);
+            var encodedData = new URLSearchParams(formData).toString();
+            xhr.send(encodedData);
+        }
+    </script>
 </body>
 </html>
+
 EOL
 
 # Run the app
